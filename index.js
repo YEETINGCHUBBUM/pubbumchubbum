7f7f8bf2e520048cd441db382d75d38b5f2837b9
YEETINGCHUBBUM <73912694+YEETINGCHUBBUM@users.noreply.github.com>
2020-11-03 18:30:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const prefix = '!';
-
+var x;
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
@@ -14,6 +14,24 @@ client.on('message', message =>{
     }
     if(command === 'cringe'){
       message.channel.send("Yeet", {files: ["cringe.png"]});
+    }
+    if(command === 'annoy' && message.member.roles.find(r => r.name === "KING")){
+        message.channel.send("@everyone");
+    }
+    if(command === 'suicide'){
+      x = Math.random()
+        if(x < 0.25){
+            message.channel.send("The knives are in the kitchen");
+        }
+        else if(x < 0.5){
+            message.channel.send("The medicine is in the cabinet.");
+        }
+        else if(x < 0.75){
+            message.channel.send("The car is in the driveway.");
+        }
+        else if(x < 1){
+            message.channel.send("The tide pods are in the bathroom.");
+        }
     }
 });
 
