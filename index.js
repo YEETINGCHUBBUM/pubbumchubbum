2520a127aaf30ef5ee9eb1df327a4018f1d33941
YEETINGCHUBBUM <73912694+YEETINGCHUBBUM@users.noreply.github.com>
2020-11-04 09:49:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ client.on('message', message =>{
     }
  
    if(command[0] === 'a' && command[1] === 'x' && command[2] === 'x'){
-      annoylisted.push(command.slice(2,command.slice));
+      annoylisted.push(command.slice(3,command.slice));
    }
 });
 
