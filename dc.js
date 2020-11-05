0b27e8a3c416a118d49167fda9895b8240b42b8e
YEETINGCHUBBUM <73912694+YEETINGCHUBBUM@users.noreply.github.com>
2020-11-05 11:46:34 -0500

    Create dc.js

---
+++ dc.js
@@ -0,0 +1,66 @@
+function yeet(){
+  var x = Math.floor(Math.random*20);
+  switch(x){
+  case 0:
+  return "https://i.redd.it/8t6n4zlkk1w51.png";
+  break;
+  case 1:
+  return "https://preview.redd.it/kl3brnuq5ru51.jpg?width=640&crop=smart&auto=webp&s=0296292817c7a677e97298201e1e86307714c516";
+  break;
+  case 2:
+  return "https://preview.redd.it/tjr877s3znu51.jpg?width=640&crop=smart&auto=webp&s=d5a377228ae1509352886b9306c6db7fb6c99679";
+  break;
+  case 3:
+  return "https://i.redd.it/vme4fgo2tnu51.jpg";
+  break;
+  case 4:
+  return "https://i.redd.it/8h3as2qltnu51.png";
+  break;
+  case 5:
+  return "https://preview.redd.it/gentpo554au51.jpg?width=640&crop=smart&auto=webp&s=6fee4c6b62dd437cf9e8ee93069bbacf0447e121";
+  break;
+  case 6:
+  return "https://preview.redd.it/00l3sqg1p9u51.png?width=640&crop=smart&auto=webp&s=3eb00e588007225a81ec5f755e0de480d0b74738";
+  break;
+  case 7:
+  return "https://preview.redd.it/4i6sa5bru8u51.jpg?width=640&crop=smart&auto=webp&s=e9103071bb335011279b1cc01ad05d78627dc055";
+  break;
+  case 8:
+  return "https://preview.redd.it/gj5jtperazt51.jpg?width=640&crop=smart&auto=webp&s=9f1d7c3ab6514362b51fefd93c8b3127b40538d2";
+  break;
+  case 9:
+  return "https://preview.redd.it/bot7dpddjrt51.jpg?width=640&crop=smart&auto=webp&s=3c0bb03065be0723370c807e6cd30157558881a4";
+  break;
+  case 10:
+  return "https://preview.redd.it/3k22o184djt51.jpg?width=640&crop=smart&auto=webp&s=8eb194dd2083ce450a9c9f44412a5700090a461e";
+  break;
+  case 11:
+  return "https://preview.redd.it/2now7vbswct51.jpg?width=640&crop=smart&auto=webp&s=4e891f29a7f4185358ce3a38498e8de7f8291b3b";
+  break;
+  case 12:
+  return "https://preview.redd.it/i3l7kbwbhys51.jpg?width=640&crop=smart&auto=webp&s=f5fd8c882dc5bb796e4cb765faa45d24d9c8714e";
+  break;
+  case 13:
+  return "https://preview.redd.it/15il4xsyojs51.jpg?width=640&crop=smart&auto=webp&s=e4422ab5c13e3eb2f9618e6495b3f71a948eda6e";
+  break;
+  case 14:
+  return "https://i.redd.it/gerzfhxoodr51.jpg";
+  break;
+  case 15:
+  return "https://preview.redd.it/k3ra0fkfvwo51.jpg?width=640&crop=smart&auto=webp&s=ff61d39958d43a072e868b5cd9e81a12424f1627";
+  break;
+  case 16:
+  return "https://preview.redd.it/ftw4ohukjqo51.jpg?width=640&crop=smart&auto=webp&s=1c5558eda79d58a97862c10990a5d8f71cbd35d5";
+  break;
+  case 17:
+  return "https://preview.redd.it/f0subopjcso51.jpg?width=640&crop=smart&auto=webp&s=2d3b2087b1ba760537ba761925e5a157ef9c4a2d";
+  break;
+  case 18:
+  return "https://preview.redd.it/h6n3mu5w31n51.jpg?width=640&crop=smart&auto=webp&s=d48679bbc246531a1830910f8171834654bbbd3d";
+  break;
+  case 19:
+  return "https://preview.redd.it/by72zcsx7fx51.png?width=640&crop=smart&auto=webp&s=b036d8c3485aeb8e988e24573b509504006fb623";
+  break;
+  }
+}
+module.exports.yeet = yeet;
