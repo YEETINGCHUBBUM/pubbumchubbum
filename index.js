060a4dc113af3db01a1800249591e5bf85138bea
YEETINGCHUBBUM <73912694+YEETINGCHUBBUM@users.noreply.github.com>
2020-11-10 18:57:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -98,8 +98,7 @@ client.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     if(command === 'test'){
-      var a = Config.findOneAndUpdate({guildID: message.guild.id},{kingrole: 'GOD'},{new: true});
-       return a.save();
+ Config.findOneAndUpdate({guildID: message.guild.id},{kingrole: 'GOD'},{new: true}).save();
     }
     if(command === 'ping'){
         message.channel.send('pong!');
