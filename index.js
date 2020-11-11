220b7828dfdf9728f48c73725a11051a0c4226ea
YEETINGCHUBBUM <73912694+YEETINGCHUBBUM@users.noreply.github.com>
2020-11-11 17:51:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -103,11 +103,7 @@ client.on('message',async message =>{
     }
     if(command === 'admintest'){
         var a = await Config.findOne({guildID: message.guild.id}).kingrole;
-        var b = 0;
-        if(message.member.roles.cache.find(r => r.name == a)){
-            b++;
-         }
-        if(b == 1){
+        if(message.member.roles.cache.find(r => r.name.toLowerCase() == a)){
             message.channel.send("Pass");
         }
         else{
