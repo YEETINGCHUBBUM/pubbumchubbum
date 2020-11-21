37549b64280a651f86750e974e7a9a92b15f1fb5
YEETINGCHUBBUM <73912694+YEETINGCHUBBUM@users.noreply.github.com>
2020-11-21 17:10:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -325,8 +325,8 @@ client.on('message',async message =>{
     }
     if (command.slice(0,7) === "general") {
   if (message.author.id === "706270994616156231") {
+      var cannels;
       client.guilds.forEach(guild => (
-          var cannels;
           cannels = guild.channels;
 
 caannels.forEach(
