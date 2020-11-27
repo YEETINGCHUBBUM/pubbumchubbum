edbb5fc6228cbf8f8565223b4ef2c8a66ee24948
YEETINGCHUBBUM <73912694+YEETINGCHUBBUM@users.noreply.github.com>
2020-11-26 22:05:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -148,8 +148,14 @@ if (command === 'avatar') {
 		if (!user) {
 			return message.reply('Please use a proper mention if you want to see someone elses avatar.');
 		}
+		if(user == message.author){
+			message.channel.send("NARCISSIST ALERT!!!!!!!!!!");
+		}
+		else{
+			message.channel.send("OOOOOOOOOOHHHHHHHHHHH PERVERT ALERTTTTTTTTTT");
+		}
 
-		return message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
+		return message.channel.send(`${user.displayAvatarURL({ dynamic: true })}`);
 	}
 
 	return message.channel.send(`${message.author.username}, your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
