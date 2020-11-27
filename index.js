46ff59c58d30ba1c120086c4553bb6cb0a25b756
YEETINGCHUBBUM <73912694+YEETINGCHUBBUM@users.noreply.github.com>
2020-11-26 22:11:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -148,17 +148,11 @@ if (command === 'avatar') {
 		if (!user) {
 			return message.reply('Please use a proper mention if you want to see someone elses avatar.');
 		}
-		if(user == message.author){
-			message.channel.send("NARCISSIST ALERT!!!!!!!!!!");
-		}
-		else{
 			message.channel.send("OOOOOOOOOOHHHHHHHHHHH PERVERT ALERTTTTTTTTTT");
-		}
-
 		return message.channel.send(`${user.displayAvatarURL({ dynamic: true })}`);
 	}
 
-	return message.channel.send(`${message.author.username}, your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+	return message.channel.send("NARCISSIST ALERT!!!!!!!!!",`${message.author.displayAvatarURL({ dynamic: true })}`);
 }
     if(command === 'admintest'){
         let a = await Config.findOne({guildID: message.guild.id});
